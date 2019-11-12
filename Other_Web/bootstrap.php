cd261c4fe8b3de8e3c7284131233ec3f0f00d8ab
@lukastham2018sis <lukas.tham.2018@sis.smu.edu.sg>
2019-11-12 23:42:14 +0800

    Deleted other folders

--- Other_Web/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-# edit the file included below. the bootstrap logic is there
-require_once 'model/bootstrap-process.php';
-$message = doBootstrap();
-$_SESSION['message'] = $message;
-header('Location:admin.php');
-?>
